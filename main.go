0e4cfcbc84ebd9a266e0385d69265cd271ccb9e6
Heron Ximenes <pokycelit@gmail.com>
2026-03-02 20:54:00 -0300

    adding basic gtk+libadwaita window

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"os/exec"
 
+	"github.com/diamondburned/gotk4-adwaita/pkg/adw"
+	"github.com/diamondburned/gotk4/pkg/gio/v2"
 	"github.com/nellfs/6502toy/internal/cpu"
 )
 
@@ -16,6 +18,16 @@ func run(name string, args ...string) error {
 }
 
 func main() {
+	// window setup
+	app := adw.NewApplication("com.nellfs.6502toy", gio.ApplicationFlagsNone)
+
+	app.ConnectActivate(func() { activate(app) })
+	if code := app.Run(os.Args); code > 0 {
+		os.Exit(code)
+	}
+
+	// code compilation
+
 	// assemble
 	err := run("ca65", "test/minimal.asm", "-o", "build/minimal.o")
 	if err != nil {
@@ -29,6 +41,8 @@ func main() {
 
 	fmt.Println("compiled to program.bin")
 
+	// cpu runtime
+
 	cpu := &cpu.CPU{}
 	cpu.PC = 0x8000
 	data, err := os.ReadFile("build/program.bin")
@@ -38,5 +52,13 @@ func main() {
 	copy(cpu.Mem[0x8000:], data)
 	cpu.Step()
 
-	fmt.Printf("A = %02X\n", cpu.A)
+	fmt.Printf("accumulator = %02X\n", cpu.A)
+}
+
+func activate(app *adw.Application) {
+	win := adw.NewApplicationWindow(&app.Application)
+	win.SetTitle("6502 Toy")
+	win.SetSizeRequest(320, 320)
+	win.SetDefaultSize(800, 600)
+	win.Present()
 }
